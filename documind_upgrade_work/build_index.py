11c0f88899d8182d0003aca6b0024ef3dca86670
TheAnikDhiman <dhimananik2629@gmail.com>
2026-09-12 14:26:45 +0530

    chore: remove development artifacts

--- documind_upgrade_work/build_index.py
+++
@@ -1,8 +0,0 @@
-"""
-DocuMind Root FAISS Build Script Wrapper
-Run: python build_index.py --chunks 10000 --index-type HNSW
-"""
-from backend.build_index import main
-
-if __name__ == "__main__":
-    main()
